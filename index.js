6879a3f16652ee1be6ddeacc36f6f06d30916e14
“Liam <okeefe.liam@yahoo.com>
2022-09-14 22:24:18 -0500

    commit tree 1348c71f3a3d1a2a4adaff0b8999260ee3d727ea

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function superbowlWin(record) {
+  let year = win.year;
+  if (win.result === "W") {
+    return win.year === "2015" } 
+}
+
+function superbowlWin(win) {
+  let year = win.year;
+  if (win.result === "W") {
+    return win.year;    } 
+    else {
+    return undefined;
+  }
+}
